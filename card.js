db7b929f060caa85cbd5b36a4fa8c076269f1492
chrisl <chrisl@pageuppeople.com>
2017-07-20 16:58:14 +1000

    You can now check off passing cases

--- card.js
+++ card.js
@@ -1,9 +1,22 @@
 var Card = React.createClass(
 {
+    getInitialState: function()
+    {
+        return { passing: false };
+    },
+
+    onCheckOff: function(event)
+    {
+        this.setState(function (previousState)
+        {
+            return { passing: !previousState.passing };
+        });
+    },
+
     render: function()
     {
         return React.createElement('div', null, 
-            React.createElement('div', { className: 'card-wrapper' },
+            React.createElement('div', { className: 'card-wrapper ' + (this.state.passing === true ? 'passing' : '') },
                 React.createElement('div', { className: 'gwt-card' },
                     React.createElement('div', { className: 'remove-card' },
                         React.createElement('i', { className: 'button fa fa-times-circle fa-2x', onClick: this.props.onDeleteCard })
@@ -12,7 +25,7 @@ var Card = React.createClass(
                     React.createElement(Group, { type: 'when', rows: this.props.when  }),
                     React.createElement(Group, { type: 'then', rows: this.props.then  }),
                     React.createElement('div', { className: 'check-off' },
-                        React.createElement('i', { className: 'button fa fa-square-o fa-2x' })
+                        React.createElement('i', { className: 'button fa fa-2x ' + (this.state.passing === true ? 'fa-check-square-o' : 'fa-square-o'), onClick: this.onCheckOff })
                     )
                 )
             ),
